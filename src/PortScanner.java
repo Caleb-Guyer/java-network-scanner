188bad3942596a8b74642a8fc17faa2619926a67
Caleb-Guyer <guyercaleb067497@gmail.com>
2026-06-11 13:55:24 -0500

    Add service name detection for common ports

--- src/PortScanner.java
+++ src/PortScanner.java
@@ -15,12 +15,16 @@ public class PortScanner
 
                 socket.connect(new InetSocketAddress(ip, i), 200);
 
-                System.out.println("  [+] OPEN PORT: " + i);
+                String service = getServiceName(i);
+
+                System.out.println("  [+] OPEN PORT: " + i + " (" + service + ")");
                 count++;
 
                 report.append("  [+] OPEN PORT: ")
                         .append(i)
-                        .append("\n");
+                        .append(" (")
+                        .append(service)
+                        .append(")\n");
 
                 socket.close();
             }
@@ -31,5 +35,41 @@ public class PortScanner
         }
 
         return count;
+    }
+
+    public static String getServiceName(int port)
+    {
+        if (port == 21)
+        {
+            return "FTP";
+        }
+        else if (port == 22)
+        {
+            return "SSH";
+        }
+        else if (port == 25)
+        {
+            return "SMTP";
+        }
+        else if (port == 80)
+        {
+            return "HTTP";
+        }
+        else if (port == 443)
+        {
+            return "HTTPS";
+        }
+        else if (port == 3389)
+        {
+            return "RDP";
+        }
+        else if (port == 8080)
+        {
+            return "HTTP-ALT";
+        }
+        else
+        {
+            return "Unknown";
+        }
     }
 }
